ad555c305ad27709c5e7fa6520bab49ebfa46ddc
Vitali Dzikavitski <vitaldikov@gmail.com>
2021-12-04 03:28:17 +0300

    add tabs check script

---
+++ Itmo.Dormitory/wwwroot/js/nav-bar.js
@@ -0,0 +1,11 @@
+﻿function setActiveNavItem(item_id) {
+    var tabs = document.querySelectorAll('.navbar-item');
+    tabs.forEach(function (el) {
+        var square = el.getElementsByClassName('check-square')[0]
+        square.style.background = 'transparent'
+    });
+    activeNavItem = document.getElementById(item_id)
+    var square = activeNavItem.getElementsByClassName('check-square')[0]
+    square.style.background = '#123456'
+}
+
