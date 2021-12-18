f2bcba1eed3696c5599ee150e9111a8aa73a9117
ALEXXXANDRO <alkolegov2011@yandex.ru>
2021-12-18 15:09:01 +0300

    feat: add repository

---
+++ Itmo.Dormitory.Infrastruture/Repositories/GenericRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Itmo.Dormitory.Data.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public GenericRepository(DbContext context, DbSet<TEntity> set)
+        {
+            _context = context;
+            _dbSet = set;
+        }
+
+        public IQueryable<TEntity> Get()
+        {
+            return _dbSet;
+        }
+
+        public IQueryable<TEntity> Get(Func<TEntity, bool> predicate)
+        {
+            return _dbSet.Where(predicate).AsQueryable();
+        }
+        public TEntity FindById(Guid id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public TEntity Create(TEntity item)
+        {
+            _dbSet.Add(item);
+            _context.SaveChanges();
+            return item;
+        }
+        public TEntity Update(TEntity item)
+        {
+            _dbSet.Update(item);
+            _context.SaveChanges();
+            return item;
+        }
+
+        public void Remove(TEntity item)
+        {
+            _dbSet.Remove(item);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Itmo.Dormitory.Infrastruture/Repositories/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Itmo.Dormitory.Data.Repositories
+{
+    public interface IGenericRepository<TEntity>
+       where TEntity : class
+    {
+        TEntity Create(TEntity item);
+        TEntity FindById(Guid id);
+        IQueryable<TEntity> Get();
+        IQueryable<TEntity> Get(Func<TEntity, bool> predicate);
+        void Remove(TEntity item);
+        TEntity Update(TEntity item);
+    }
+}
