3da920514ea5ebd32354cb38d6c4cbdd888d344f
ALEXXXANDRO <alkolegov2011@yandex.ru>
2021-12-19 13:20:40 +0300

    feat: add db context

---
+++ Itmo.Dormitory.Infrastruture/Contexts/DormitoryContext.cs
@@ -0,0 +1,24 @@
+﻿using Itmo.Dormitory.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Itmo.Dormitory.Data.Contexts
+{
+    public class DormitoryContext : DbContext
+    {
+        public DbSet<Announcement> Announcements { get; set; }
+        public DbSet<Application> Applications { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+
+        public DormitoryContext(DbContextOptions<DormitoryContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=database.db");
+        }
+        public DormitoryContext()
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
