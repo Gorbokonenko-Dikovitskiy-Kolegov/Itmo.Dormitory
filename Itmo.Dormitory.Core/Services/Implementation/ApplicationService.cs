1e2d0f19c9c87580fc2313fd59af09c72d5c07bb
ALEXXXANDRO <alkolegov2011@yandex.ru>
2021-12-23 19:57:59 +0300

    chore: add packages, move some files

---
+++ Itmo.Dormitory.Core/Services/Implementation/ApplicationService.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Itmo.Dormitory.Core.DTO;
+using Itmo.Dormitory.Core.Services.Interfaces;
+using Itmo.Dormitory.Data.Entities;
+using Itmo.Dormitory.Data.Repositories;
+using System;
+using System.Collections.Generic;
+
+namespace Itmo.Dormitory.Core.Services.Implementation
+{
+    public class ApplicationService : IApplicationService
+    {
+        private readonly GenericRepository<Application> _applicationRepository;
+        private readonly IMapper _toDTOmapper;
+        private readonly IMapper _fromDTOmapper;
+        public ApplicationService(GenericRepository<Application> applicationRepository)
+        {
+            _applicationRepository = applicationRepository;
+            _toDTOmapper = new MapperConfiguration(cfg => cfg.CreateMap<Application, ApplicationDTO>()).CreateMapper();
+            _fromDTOmapper = new MapperConfiguration(cfg => cfg.CreateMap<ApplicationDTO,Application>()).CreateMapper();
+        }
+        public void AddApplication(ApplicationDTO application)
+        { 
+            _applicationRepository.Add(_fromDTOmapper.Map<ApplicationDTO, Application>(application));
+        }
+        public ApplicationDTO GetApplication(Guid id)
+        {
+            var application = _applicationRepository.FindById(id);
+            return _toDTOmapper.Map<Application, ApplicationDTO>(application);
+        }
+        public void RemoveApplication(Guid id)
+        {
+            _applicationRepository.Remove(id);
+        }
+
+        public IEnumerable<ApplicationDTO> GetAllApplications()
+        {
+            return _toDTOmapper.Map<IEnumerable<Application>, List<ApplicationDTO>>(_applicationRepository.GetAll());
+        }
+    }
+}
---
+++ Itmo.Dormitory.Core/Services/Interfaces/IApplicationService.cs
@@ -0,0 +1,14 @@
+﻿using Itmo.Dormitory.Core.DTO;
+using System;
+using System.Collections.Generic;
+
+namespace Itmo.Dormitory.Core.Services.Interfaces
+{
+    public interface IApplicationService
+    {
+        public void AddApplication(ApplicationDTO application);
+        public ApplicationDTO GetApplication(Guid id);
+        public void RemoveApplication (Guid id);
+        public IEnumerable<ApplicationDTO> GetAllApplications();
+    }
+}
